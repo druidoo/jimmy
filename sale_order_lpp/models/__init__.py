c7cf9177327637901ba6a26c2d978a3d4321fdf9
Abdessamad HILALI <abdessamad.hilali@arkeup.com>
2022-03-18 17:16:17 +0100

    [ADD] modules

---
+++ sale_order_lpp/models/__init__.py
@@ -0,0 +1,7 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import sale_order
+from . import res_company
+from . import res_users
+from . import product_template
+from . import sale_order_line
+
