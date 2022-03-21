848cc904a8d1198feb2b6280fcfdd0bdcb6d514b
ahilali-arkeup <93914365+ahilali-arkeup@users.noreply.github.com>
2022-03-21 16:33:17 +0100

    Update models.py

--- customer_reviews/models/models.py
+++ customer_reviews/models/models.py
@@ -95,7 +95,7 @@ class CustomerReview(models.Model):
     _rec_name = 'partner_id'
 
     review_type = fields.Selection([("order", "order"), ("after 3 months", "After 3 months")], string="Type")
-    order_id = fields.Many2one("res.partner", string="Order")
+    order_id = fields.Many2one("sale.order", string="Order")
     partner_id = fields.Many2one("res.partner", string="Customer", required=True)
     message = fields.Text(string="Message")
     ratings = fields.Selection(
